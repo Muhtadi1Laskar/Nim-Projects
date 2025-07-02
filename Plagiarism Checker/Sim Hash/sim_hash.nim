a639d0d9080879d204f736eaef01e8e7f318e6b8
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-02 15:58:46 +0600

    Added the function to calculate hamming distance

--- Plagiarism Checker/Sim Hash/sim_hash.nim
+++ Plagiarism Checker/Sim Hash/sim_hash.nim
@@ -1,21 +1,16 @@
 import std/[strutils, sequtils, math, re]
 import nimcrypto
 
-proc hash64(word: string): uint64 = 
-  let digest = sha256.digest(word)
-  var result: uint64
-
-  for i in 0 ..< 8:
-    result = (result shl 8) or uint64(digest.data[i])
-
-  return result
+proc hash64(word: string): uint64 =
+    let digest = sha256.digest(word)
+    for i in 0 ..< 8:
+      result = (result shl 8) or uint64(digest.data[i])
 
 proc sim_hash(text: string): uint64 = 
-  let 
-    words = text
-      .toLowerAscii()
-      .replace(re"[^\w\s]", "")
-      .splitWhitespace()
+  let words = text
+    .toLowerAscii()
+    .replace(re"[^\w\s]", "")
+    .splitWhitespace()
     
   if words.len == 0:
     return 0'u64
@@ -25,26 +20,32 @@ proc sim_hash(text: string): uint64 =
   for word in words:
     let h = hash64(word)
     for i in 0 ..< 64:
-      if((h shr i) and 1) == 1:
-        vector[i] += 1
-      else:
-        vector[i] = 1
-  
-  var final_hash: uint64 = 0
-  for i in 0 ..< 64:
-    if vector[i] > 0:
-      final_hash = final_hash or (1'u64 shl i)
-  
-  return final_hash
+      vector[i] += ((h shr i) and 1).int * 2 - 1  # +1 for 1, -1 for 0
 
+  for i in 0 ..< 64:
+    if vector[i] > 0:  # More 1s than 0s across all words
+      result = result or (1'u64 shl i)
+
+proc hamming_distance(n: uint64): int =
+  result = 0
+  var x = n
+
+  while x != 0:
+    result += int(x and 1)
+    x = x shr 1
 
 when isMainModule:
-  let text1 = "The quick brown fox jumped over the lazy dog"
-  let text2 = "The quick brown fox jumped over the lazy dog"
+  let text1 = "Apples are sweet and tasty"
+  let text2 = "Apples are tasty and sweet"
+  let text3 = "Bananas are yellow and soft"
 
   let sim_hash1 = sim_hash(text1)
   let sim_hash2 = sim_hash(text2)
+  let sim_hash3 = sim_hash(text3)
 
   echo ""
   echo "Hash ", sim_hash1
   echo "Hash ", sim_hash2
+  echo "Hash ", sim_hash3
+
+  echo hamming_distance(sim_hash1 xor sim_hash3)
