a61d805ddf9d65378c15a7a6f748aeeee34d50a4
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-03 16:17:05 +0600

    Added load data function

--- Change Detection/ChangeDetection.nim
+++ Change Detection/ChangeDetection.nim
@@ -1,5 +1,8 @@
 import  std/[os, streams, strutils, json, jsonutils, tables]
+import nimcrypto
 import ../Common/FileOperations
+
+const ChunkSize = 8192
 
 proc load_data(path: string): Table[string, string] = 
     let json_data = FileOperations.read_file(path)
@@ -15,11 +18,37 @@ proc load_data(path: string): Table[string, string] =
     
     return t
 
-proc 
+proc hash_data(path: string): string = 
+    var stream = newFileStream(path, fmRead)
+    if stream == nil:
+        raise newException(IOError, "❌ Failed to open: " & path)
+
+    var ctx: sha256
+    ctx.init()
+    var buffer: array[ChunkSize, byte]
+
+    while not stream.atEnd():
+        let read = stream.readData(buffer.addr, ChunkSize)
+        if read > 0:
+            ctx.update(buffer[0 ..< read])
+    stream.close()
+
+    let digest = ctx.finish()
+    result = toHex(digest.data, lowercase = true)
+
 
 
 when isMainModule:
-    let data = load_data("./JsonData/hashes.json")
+    let files_path = FileOperations.get_all_files("./Data")
+    var file_array: seq[Table[string, string]]
+    var file_hash_table = initTable[string, string]()
+
+    for path in files_path:
+        var hash = hash_data(path)
+
+        if not file_hash_table.hasKey(path):
+            file_hash_table[path] = hash
     
-    echo data.len
+    for k, v in file_hash_table.pairs:
+        echo k, v
 
