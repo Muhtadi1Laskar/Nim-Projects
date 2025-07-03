9c3f267fa1fa730ccfa312290523922edb37d7a2
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-03 15:57:02 +0600

    Added load data function

--- Change Detection/ChangeDetection.nim
+++ Change Detection/ChangeDetection.nim
@@ -1,4 +1,25 @@
+import  std/[os, streams, strutils, json, jsonutils, tables]
+import ../Common/FileOperations
+
+proc load_data(path: string): Table[string, string] = 
+    let json_data = FileOperations.read_file(path)
+    let node = parseJson(json_data)
+
+    if node.len == 0:
+        echo "The file is empty"
+        return
+
+    var t: Table[string, string]
+    for k, v in node.pairs:
+        t[k] = v.getStr()
+    
+    return t
+
+proc 
 
 
 when isMainModule:
-    echo "Hello World"
+    let data = load_data("./JsonData/hashes.json")
+    
+    echo data.len
+
