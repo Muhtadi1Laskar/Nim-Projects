f2d74b36bac7f3845b6e5ff96fd4c4b7829fd387
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-05 16:07:21 +0600

    Added the function to create block and print each item

--- Blockchain/BlockChain.nim
+++ Blockchain/BlockChain.nim
@@ -1,5 +1,6 @@
 import std/[strutils, times, tables]
 import nimcrypto
+import ../Common/FileOperations
 
 type
     Block = ref object
@@ -7,4 +8,35 @@ type
         time_stamp: string
         proof: int
         previous_hash: string
+    
+type
+    Chain = ref object
+        chain: seq[Block]
 
+proc create_block(self: Chain, proof: int, previous_hash: string): Block =
+    let new_block = Block(
+        index: self.chain.len + 1,
+        time_stamp: FileOperations.get_date_time(),
+        proof: proof,
+        previous_hash: previous_hash
+    )
+    self.chain.add(new_block)
+
+    return new_block
+
+proc new_block_chain(): Chain = 
+    let c = Chain(chain: @[])
+    discard c.create_block(proof = 1, previousHash = "0")
+    return c
+
+proc `$`(self: Block): string =
+  result = "Block(" &
+           "index: " & $self.index & ", " &
+           "time_stamp: " & self.time_stamp & ", " &
+           "proof: " & $self.proof & ", " &
+           "previous_hash: " & self.previous_hash & ")"
+
+
+when isMainModule:
+    let block_chain = new_block_chain()
+    echo block_chain.chain
