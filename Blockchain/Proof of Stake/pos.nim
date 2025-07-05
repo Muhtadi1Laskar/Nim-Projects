5ffe49ad849272b02d7ef2f4567953587334c416
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-06 03:50:15 +0600

    Added the create_genesis_block() funtion for pos

---
+++ Blockchain/Proof of Stake/pos.nim
@@ -0,0 +1,49 @@
+import std/[tables]
+import nimcrypto
+import ../../Common/FileOperations
+
+type
+    Block = ref object
+        index: int
+        time_stamp: string
+        data: string
+        previous_hash: string
+        validator: string
+        hash: string
+
+type
+    Chain = ref object
+        chain: seq[Block]
+        validators: Table[string, string]
+
+
+proc create_genesis_block(): Block = 
+    return Block(
+        index: 0,
+        time_stamp: FileOperations.get_date_time(),
+        data: "Genesis Block",
+        previous_hash: "0",
+        validator: "None"
+    )
+
+proc new_chain(): Chain =
+    result = Chain(
+        chain: @[create_genesis_block()], 
+        validators: initTable[string, string]()
+    )
+
+proc `$`(b: Block): string =
+  result = "Block(" &
+           "index: " & $b.index & ", " &
+           "time_stamp: " & b.time_stamp & ", " &
+           "validator: " & b.validator & ", " &
+           "previous_hash: " & b.previous_hash & ")"
+
+when isMainModule:
+    let block_chain = new_chain()
+
+    for blocks in block_chain.chain:
+        echo blocks
+
+    
+
