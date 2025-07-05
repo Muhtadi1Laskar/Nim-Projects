c3f41cbe78d3229153df5982e2d9e1784497b80f
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-06 04:08:48 +0600

    Added the function to add validator to the chain

--- Blockchain/Proof of Stake/pos.nim
+++ Blockchain/Proof of Stake/pos.nim
@@ -14,7 +14,7 @@ type
 type
     Chain = ref object
         chain: seq[Block]
-        validators: Table[string, string]
+        validators: Table[string, int]
 
 
 proc create_genesis_block(): Block = 
@@ -23,17 +23,23 @@ proc create_genesis_block(): Block =
         time_stamp: FileOperations.get_date_time(),
         data: "Genesis Block",
         previous_hash: "0",
-        validator: "None"
+        validator: "None",
     )
 
-proc calculate_hash(self: Chain, block_data: Block): string = 
+proc add_validator(self: Chain, name: string, stake: int) = 
+    self.validators[name] = stake
+
+proc calculate_hash(block_data: Block): string = 
     let record = $block_data.time_stamp & $block_data.data & $block_data.previous_hash & block_data.validator
     return toHex(sha512.digest(record).data)
 
 proc new_chain(): Chain =
+    let genesis_block = create_genesis_block()
+    genesis_block.hash = calculate_hash(genesis_block)
+
     result = Chain(
-        chain: @[create_genesis_block()], 
-        validators: initTable[string, string]()
+        chain: @[genesis_block], 
+        validators: initTable[string, int]()
     )
 
 proc `$`(b: Block): string =
@@ -41,14 +47,20 @@ proc `$`(b: Block): string =
            "index: " & $b.index & ", " &
            "time_stamp: " & b.time_stamp & ", " &
            "validator: " & b.validator & ", " &
+           "hash: " & b.hash & ", " &
            "previous_hash: " & b.previous_hash & ")"
 
 when isMainModule:
     let block_chain = new_chain()
 
-    for blocks in block_chain.chain:
-        let h = block_chain.calculate_hash(blocks)
-        echo h
+    block_chain.add_validator("Alice", 50)
+    block_chain.add_validator("Bob", 30)
+    block_chain.add_validator("Charlie", 20)
+
+    # for blocks in block_chain.chain:
+    #     echo blocks
+
+    echo block_chain.validators
 
     
 
