e4f908e2fe36633677adfc18bea14abc54061789
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-10 04:38:10 +0600

    Implemented insert method for the auto-complete project

--- Auto-Comlete Sytem/AutoCompleteBst.nim
+++ Auto-Comlete Sytem/AutoCompleteBst.nim
@@ -11,8 +11,26 @@ type
 proc new_tree(): Tree = 
     return Tree(root: nil)
 
+proc insert_helper(bst: Tree, value: string) =
+    if bst.value < value:
+        if node.left.isNil:
+            node.left = Node(value: value)
+        else:
+            bst.insert_helper(node.left, value)
+    else:
+        if node.right.isNil:
+            node.right = Node(value: value)
+        else: 
+            bst.insert_helper(node.right, value)
+    
+
+proc insert(bst: Tree, value: string) = 
+    if bst.root.isNil:
+        bst.root = Node(value: value)
+        return
+    else:
+        bst.insert_helper(bst.root, value)
 
 when isMainModule:
     let tree = new_tree()
 
-    
