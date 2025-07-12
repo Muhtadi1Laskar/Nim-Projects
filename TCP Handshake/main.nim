5335415a4bfce304888407732e83af4721d38b90
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-12 15:16:36 +0600

    Updated the code

--- TCP Handshake/main.nim
+++ TCP Handshake/main.nim
@@ -1,7 +1,7 @@
 import asyncnet, asyncdispatch, strutils
 
 proc handleConnection(sock: AsyncSocket) {.async.} =
-  let syn = await sock.recvLine()
+  let syn: string = await sock.recvLine()
   if syn.strip() == "SYN":
     echo "Server received SYN from the client"
     await sleepAsync(1000)
@@ -9,33 +9,33 @@ proc handleConnection(sock: AsyncSocket) {.async.} =
     echo "Server sending SYN-ACK to the client"
     await sock.send("SYN-ACK\n")
 
-    let ack = await sock.recvLine()
+    let ack: string = await sock.recvLine()
     if ack.strip() == "ACK":
       echo "Server received ACK from the client"
     else:
       echo "Invalid response from client: ", ack.strip()
 
 proc startServer() {.async.} =
-  let server = newAsyncSocket()
+  let server: AsyncSocket = newAsyncSocket()
   await server.bindAddr(Port(8080))
   server.listen()
 
   echo "Server started. Listening on :8080"
 
   while true:
-    let client = await server.accept()
+    let client: AsyncSocket = await server.accept()
     asyncCheck handleConnection(client)
 
 proc startClient() {.async.} =
   await sleepAsync(1000)  # Ensure server is up
 
-  let client = newAsyncSocket()
+  let client: AsyncSocket = newAsyncSocket()
   await client.connect("127.0.0.1", Port(8080))
 
   echo "Client sending SYN to the server"
   await client.send("SYN\n")
 
-  let synAck = await client.recvLine()
+  let synAck: string = await client.recvLine()
   if synAck.strip() == "SYN-ACK":
     echo "Client received SYN-ACK from the server"
     await sleepAsync(1000)
