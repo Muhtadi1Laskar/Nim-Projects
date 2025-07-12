34ccaf7ac621ec09df51fc9b501c36d16a0ea8ab
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-12 18:45:08 +0600

    Added the secure password function

--- Password/GeneratePassword.nim
+++ Password/GeneratePassword.nim
@@ -1,10 +1,35 @@
-import std/[random]
+import std/[random, strutils]
+
+const 
+    UPPER: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    LOWER: string = "abcdefghijklmnopqrstuvwxys"
+    DIGITS: string = "0123456789"
+    SYMBOLS: string = "!@#$%^&*()-_=+[]{}|;:,.<>?/"
 
 
-proc generate_random_int(min, max: int): int = 
-    return min + rand(max - min)
+const CHARSET = UPPER & LOWER & DIGITS & SYMBOLS
+
+proc secure_password(length: int): string = 
+    if length < 4:
+        raise newException(ValueError, "Password length must be at least 4")
+
+    randomize()
+
+    var passwordChars: seq[char] = @[
+        UPPER[rand(UPPER.high)],
+        LOWER[rand(LOWER.high)],
+        DIGITS[rand(DIGITS.high)],
+        SYMBOLS[rand(SYMBOLS.high)]
+    ]
+
+    for _ in 4 ..< length:
+        passwordChars.add(CHARSET[rand(CHARSET.high)])
+    
+    passwordChars.shuffle()
+
+    return passwordChars.join("")
 
 when isMainModule:
-    let num: int = generate_random_int(10, 100)
+    let password: string = secure_password(10)
 
-    echo num
+    echo password
