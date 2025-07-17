c4634f2252fe8cddb57b1a74e5cd8f8d975bab50
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-18 03:55:42 +0600

    Added the function to get random bytes

--- BIP-39/main.nim
+++ BIP-39/main.nim
@@ -1,4 +1,4 @@
-import std/[strutils]
+import std/[strutils, random]
 import ../Common/FileOperations
 
 proc getWordList(): seq[string] = 
@@ -7,11 +7,18 @@ proc getWordList(): seq[string] =
     var wordList: seq[string] = data.split("\n")
     return wordList
 
+proc randomByte(len: int): seq[byte] = 
+    var randomBytes: array[16, byte]
+    randomize()
+
+    for i  in 0 ..< randomBytes.len:
+        randomBytes[i] = rand(255).byte
+
+    return @randomBytes
+
 when isMainModule:
-    let filePath = "C:/Users/laska/OneDrive/Documents/Coding/Nim/nim-projects/BIP-39/bip39.txt"
-    var data: string = FileOperations.read_file(filePath)
-
     var wordList: seq[string] = getWordList()
+    var randomNum: seq[byte] = randomByte(16)
 
-    echo wordList
+    echo randomNum
 
