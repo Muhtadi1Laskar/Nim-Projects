c4f32a3a80ef6fec154af561285e3b6709ce3dc6
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-21 04:44:43 +0600

    Added the task tacker struct

--- Task-Tracker/TaskTracker.nim
+++ Task-Tracker/TaskTracker.nim
@@ -1,1 +1,20 @@
-echo "Hello World"
+type
+    Task = ref object
+        id: int
+        description: string
+        status: string
+        createdAt: string
+        updatedAt: string
+        next: Task
+
+type
+    TaskTracker = ref object
+        head: Task
+        length: int
+
+proc newTracker(): TaskTracker =
+    return TaskTracker(head: nil, length: 0)
+
+
+when isMainModule:
+    var tracker: TaskTracker = newTracker()    
