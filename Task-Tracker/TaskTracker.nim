18fb34cf23e70d6f360eb26e65c2d2de813c7b2c
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-21 04:52:04 +0600

    Added the function to add task to the tracker:

--- Task-Tracker/TaskTracker.nim
+++ Task-Tracker/TaskTracker.nim
@@ -1,3 +1,5 @@
+import ../Common/FileOperations
+
 type
     Task = ref object
         id: int
@@ -14,6 +16,30 @@ type
 
 proc newTracker(): TaskTracker =
     return TaskTracker(head: nil, length: 0)
+
+proc append(list: TaskTracker, value: string) = 
+    list.length += 1
+
+    var newNode: Task = Task(
+        id: list.length,
+        description: value,
+        status: "todo",
+        createdAt: FileOperations.get_date_time(),
+        updatedAt: ""
+    )
+
+    if list.head.isNil:
+        list.head = newNode
+        return
+
+    var currentNode: Task = list.head
+
+    while not currentNode.next.isNil:
+        currentNode = currentNode.next
+
+    currentNode.next = newNode
+
+    return 
 
 
 when isMainModule:
