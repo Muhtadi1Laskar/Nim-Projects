502fb3c6b6edb2746506236a3b00167c06b1b701
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-08-17 18:14:04 +0600

    Added the function to calculate the fourier transformation

--- Fourier-Transformation/main.nim
+++ Fourier-Transformation/main.nim
@@ -1,1 +1,22 @@
-echo "Hello World"
+import std/[math, complex]
+
+proc Dft(signal: seq[float]): seq[Complex[float]] =
+    let N = signal.len
+    result = newSeq[Complex[float]](N)
+
+    for k in 0..<N:
+        var s = complex(0.0, 0.0)
+        for n in 0..<N:
+            let angle = -2.0 * PI * float(k * n) / float(N)
+            let w = complex(cos(angle), sin(angle))
+            s += signal[n] * w
+        result[k] = s
+
+
+when isMainModule:
+    let samples = @[1.0, 0.0, -1.0, 0.0]
+    let specturm = Dft(samples)
+
+    echo specturm
+
+
